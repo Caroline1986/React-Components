41ab5eb94e75ed8d86f5e2ac2c2e43cce110d448
Caroline E <caroline.esquivel01@gmail.com>
2021-02-11 00:14:46 -0600

    extracted css to separate component

--- src/index.js
+++ src/index.js
@@ -19,6 +19,34 @@ const App = () => {
               </div>
               <div className={"text"}>Nice Blog Post!</div>
           </div>
+          <div className={"comment"}>
+              <a href={"/"} className={"avatar"}>
+                  <img alt={"avatar"} src={faker.image.image()}/>
+              </a>
+              <div className={"content"}>
+                  <a href={"/"} className={"author"}>
+                      Sam
+                  </a>
+              </div>
+              <div className={"metadata"}>
+                  <span className={"date"}>Today at 6:00PM</span>
+              </div>
+              <div className={"text"}>Nice Blog Post!</div>
+          </div>
+          <div className={"comment"}>
+              <a href={"/"} className={"avatar"}>
+                  <img alt={"avatar"} src={faker.image.image()}/>
+              </a>
+              <div className={"content"}>
+                  <a href={"/"} className={"author"}>
+                      Sam
+                  </a>
+              </div>
+              <div className={"metadata"}>
+                  <span className={"date"}>Today at 6:00PM</span>
+              </div>
+              <div className={"text"}>Nice Blog Post!</div>
+          </div>
       </div>
     );
 };
