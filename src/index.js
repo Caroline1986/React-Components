1e7aac92e76d10e9b5912505b6bb6b4f4153a3e5
Caroline E <caroline.esquivel01@gmail.com>
2021-02-11 00:42:38 -0600

    refactored index with props

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from "faker";
 import CommentDetail from "./CommentDetail"; //renders file
 
 const App = () => {
     return (
       <div className={"ui container comments"}>
          {/* to show component in another component use jsx tag*/}
-         <CommentDetail author={"Sam"} />
-         <CommentDetail author={"Alex"} />
-         <CommentDetail author={"Jane"} />
+         <CommentDetail
+             author={"Sam"}
+             timeAgo={"Today at 4:45PM"}
+             comment={"This is a comment"}
+             avatar={faker.image.image()}
+         />
+         <CommentDetail
+             author={"Alex"}
+             timeAgo={"Today at 2:00"}
+             comment={"Hey nice"}
+             avatar={faker.image.image()}
+         />
+         <CommentDetail
+             author={"Jane"}
+             timeAgo={"Yesterday at 5:00PM"}
+             comment={"bad"}
+             avatar={faker.image.image()}
+         />
       </div>
     );
 };
