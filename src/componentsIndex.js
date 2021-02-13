f42c925e97376f4f809533062466631ccc2017b8
Caroline E <caroline.esquivel01@gmail.com>
2021-02-12 21:50:29 -0600

    added geolocation

---
+++ src/componentsIndex.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from "faker";
+import CommentDetail from "./CommentDetail"; //renders file
+import ApprovalCard from "./ApprovalCard";
+
+const App = () => {
+    return (
+        <div className={"ui container comments"}>
+           <ApprovalCard>
+               <div>
+                   <h4>Warning!</h4>
+                   Are you sure you want to do this?
+               </div>
+           </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author={"Sam"}
+                    timeAgo={"Today at 4:45PM"}
+                    comment={"This is a comment"}
+                    avatar={faker.image.image()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author={"Alex"}
+                    timeAgo={"Today at 2:00"}
+                    comment={"Hey nice"}
+                    avatar={faker.image.image()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author={"Jane"}
+                    timeAgo={"Yesterday at 5:00PM"}
+                    comment={"bad"}
+                    avatar={faker.image.image()}
+                />
+            </ApprovalCard>
+        </div>
+    );
+};
+
+// ReactDOM.render(<App/>, document.querySelector('#root'));
+
+//https://semantic-ui.com/views/comment.html : google semantic ui cdn
--- src/index.js
+++ src/index.js
@@ -1,34 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from "faker";
-import CommentDetail from "./CommentDetail"; //renders file
+import SeasonDisplay from "./SeasonDisplay";
 
 const App = () => {
-    return (
-      <div className={"ui container comments"}>
-         {/* to show component in another component use jsx tag*/}
-         <CommentDetail
-             author={"Sam"}
-             timeAgo={"Today at 4:45PM"}
-             comment={"This is a comment"}
-             avatar={faker.image.image()}
-         />
-         <CommentDetail
-             author={"Alex"}
-             timeAgo={"Today at 2:00"}
-             comment={"Hey nice"}
-             avatar={faker.image.image()}
-         />
-         <CommentDetail
-             author={"Jane"}
-             timeAgo={"Yesterday at 5:00PM"}
-             comment={"bad"}
-             avatar={faker.image.image()}
-         />
-      </div>
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        err => console.log(err)
     );
+    return <div> Hi there!</div>;
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
-
-//https://semantic-ui.com/views/comment.html : google semantic ui cdn
+ReactDOM.render(<App/>, document.querySelector('#root'));
