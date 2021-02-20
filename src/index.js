4f914f9c21451b91183d2b9574c4125c4119069f
Caroline E <caroline.esquivel01@gmail.com>
2021-02-20 00:15:50 -0600

    conditional rendering

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from "./SeasonDisplay";
-import {render} from "@testing-library/react";
 
 class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {lat: null, errorMessage: ''};
+        this.state = { lat: null, errorMessage: '' };
 
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({lat: position.coords.latitude})
+            (position) => {
+                // we called setstate!!!!
+                this.setState({ lat: position.coords.latitude });
             },
-            err => {
-                this.setState({errorMessage: err.message})
+            (err) => {
+                this.setState({ errorMessage: err.message });
             }
         );
     }
 
     render() {
-        return (
-            <div>
-                Latitude: {this.state.lat}
-                <br/>
-                Error: {this.state.errorMessage}
-            </div>
-        );
+      if (this.state.errorMessage && !this.state.lat){
+          return <div>Error: {this.state.errorMessage}</div>
+      }
+
+      if (!this.state.errorMessage && this.state.lat){
+          return <div> Latitude: {this.state.lat}</div>
+      }
+
+      return <div>Loading!</div>;
     }
 }
 
-
-ReactDOM.render(<App/>, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
