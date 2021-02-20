1d9fb1b4251b9d6c0deb41779d0a2ac4fa37d5e0
Caroline E <caroline.esquivel01@gmail.com>
2021-02-19 23:44:36 -0600

    defined constructor method and defined state

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from "./SeasonDisplay";
+import {render} from "@testing-library/react";
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        err => console.log(err)
-    );
-    return <div> Hi there!</div>;
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null};
+    }
+
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            err => console.log(err)
+        );
+        return <div>Latitude: </div>;
+    }
 };
+
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
