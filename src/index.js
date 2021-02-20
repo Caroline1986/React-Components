8f55182bfef933a68f7217307443e8456b74cef2
Caroline E <caroline.esquivel01@gmail.com>
2021-02-19 23:53:30 -0600

    set state initialized

--- src/index.js
+++ src/index.js
@@ -7,17 +7,20 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { lat: null};
+        this.state = {lat: null};
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+              this.setState({lat: position.coords.latitude})
+            },
+            err => console.log(err)
+        );
     }
 
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            err => console.log(err)
-        );
-        return <div>Latitude: </div>;
+        return <div>Latitude: {this.state.lat}</div>;
     }
-};
+}
 
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
