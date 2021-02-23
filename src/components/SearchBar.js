afece37374c63a56ed441b1807f5854455a43197
Caroline E <caroline.esquivel01@gmail.com>
2021-02-23 00:07:39 -0600

    refacotored search bar from uncontrolled to controlled

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,7 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    onInputChange(event) {
-
-    }
-
-    onInputClick() {
-
-    }
+state = { term: ''};
 
     render() {
         return (
@@ -15,7 +9,7 @@ class SearchBar extends React.Component {
                 <form className={"ui form"}>
                     <div className={"field"}>
                         <label>Image Search</label>
-                        <input type={"text"} onClick={this.onInputClick} onChange={this.onInputChange}/>
+                        <input type={"text"} value={this.state.term} onChange={(e) => this.setState({term: e.target.value})}/>
                     </div>
                 </form>
             </div>
