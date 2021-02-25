2d76e02ccff11c4e970cc6a8868feb577f9f7ef7
Caroline E <caroline.esquivel01@gmail.com>
2021-02-24 23:59:37 -0600

    images

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-axios.create({
-    baseURL: 'https://api.unsplash.com',
-    headers: {
-        Authorization: 'Client-ID ecFozKzZYjxDai1FfYoo7LnWnjVFttiSeTCugb7MLjg'
-    }
-});
