8f3900ae00ff558615e32f341d110cc4ae8346df
Caroline E <caroline.esquivel01@gmail.com>
2021-03-17 23:51:32 -0500

    initialized state

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+    state = { term: ''};
     render() {
         return <div className={"search-bar ui segment"}>
             <form className={"ui form"}>
                 <div className={"field"}>
                     <label>Video Search</label>
-                    <input type={"text"}></input>
+                    <input type={"text"} value={this.state.term}/>
                 </div>
             </form>
         </div>;
