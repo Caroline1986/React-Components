62bfe79a298bfc5cd8ee92c4916cbcdf74576dae
Caroline E <caroline.esquivel01@gmail.com>
2021-03-24 23:53:39 -0500

    imported youtube api to app.js

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -3,5 +3,11 @@ import axios from 'axios';
 const KEY = 'AIzaSyCQ1wqrgZ6IXNggF75jqwd9mYwcqNfxbN4';
 
 export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3'
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5,
+        key: KEY
+    }
 });
